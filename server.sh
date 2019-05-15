38fe3cbde488afacea2565cdc2725b102abe1a34
Miqdad Yanuar Farcha <miqdad.farcha@gmail.com>
2019-05-15 07:22:49 +0700

    Create server.sh

---
+++ server.sh
@@ -0,0 +1,26 @@
+echo "Update"
+sudo apt update
+
+echo "Installing apache2"
+sudo apt install apache2 -y
+
+echo "Installing php 5.6"
+sudo add-apt-repository ppa:ondrej/php -y
+sudo apt install php5.6 php5.6-common php5.6-mysql php5.6-mbstring php5.6-curl -y
+
+echo "Installing mariadb and phpmyadmin"
+sudo apt install mariadb-server phpmyadmin -y
+
+echo "linking phpmyadmin"
+sudo ln -s /usr/share/phpmyadmin /var/www/html
+
+echo "Clone Projects..."
+cd /var/www/html
+pwd
+git clone https://github.com/miqdadyyy/VULN
+mv VULN/* ./
+rm -rf VULN
+chmod 777 -R ./
+
+echo "Config database..."
+mysql < .secret/current.sql
